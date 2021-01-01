fix bug for dropping duplicates. macro avg precision: 0.88, macro avg recall: 0.38, macro f1: 0.46

kg/ner/baseline.py
@@ -15,9 +15,9 @@ Implementation notes:
 5) Use standard sklearn evaluation on columns of labels to evaluate.
 
 Experiments:
-1) add validation set to train_dict
-2) try lowercasing everything
-3) check implementation for bugs
+1) add validation set to train_dict - small increase for precision/recall/f1
+2) try lowercasing everything - decrease in precision. recall/f1 steady.
+3) check implementation for bugs - found bug in the way I was dropping duplicates.
 """
 
 import argparse
@@ -30,13 +30,22 @@ from sklearn.metrics import classification_report
 
 
 def create_train_dict(df):
+    # try lowercasing to improve hit rate
+    # df['Token'] = df['Token'].str.lower()
+
     temp_df = df.groupby(['NER_Tag_ID', 'NER_Tag_Normalized'],
                          as_index=False).agg(Entity=('Token', tuple))
+
+    # drop ambiguous entities
+    temp_df = temp_df.groupby(['NER_Tag_Normalized', 'Entity'],
+                              as_index=False).count()
+    temp_df = temp_df[~temp_df.duplicated(subset=['Entity'])]
+
     temp_df = temp_df[['Entity', 'NER_Tag_Normalized']]
-    # drop all duplicates
-    temp_df = temp_df.drop_duplicates(subset='Entity', keep=False)
     temp_dict = pd.Series(temp_df['NER_Tag_Normalized'].values,
                           index=temp_df['Entity']).to_dict()
+
+    # Longer phrases are preferred over shorter ones.
     initial_keys = list(temp_dict.keys())
     longest_strings = []
     for substring in initial_keys:
@@ -64,26 +73,30 @@ def get_tags(train_dict, sentence, max_key_len):
         broke = False
         for j in range(1, max_key_len):
             end_index = min(i + j, len(sentence))
+            # lowercase_sub_sentence = [x.lower() for x in sentence[i:end_index]]
+            # lowercase_sub_sentence = tuple(lowercase_sub_sentence)
             sub_sentence = tuple(sentence[i:end_index])
             if sub_sentence in train_dict:
                 entity_type = train_dict[sub_sentence]
-                tags = [(tok, entity_type) for tok in sub_sentence]
+                tags = [(tok, entity_type) for tok in sentence[i:end_index]]
                 labeled_sentence[i:end_index] = tags
                 i += j
-                broke=True
+                broke = True
                 break
             else:
-                tags = [(tok, 'O') for tok in sub_sentence]
+                tags = [(tok, 'O') for tok in sentence[i:end_index]]
                 labeled_sentence[i:end_index] = tags
         # if nothing found
         if not broke:
             i += 1
     return labeled_sentence
 
+
 def increment_id(x, increment):
     if x == np.nan:
         return np.nan
     return x + increment
+
 
 def main(args):
     file_names = ['train.csv', 'validation.csv', 'test.csv']
@@ -94,39 +107,51 @@ def main(args):
         file_path = os.path.join(args.data_directory, file_name)
         df_dict[file_name] = pd.read_csv(file_path)
 
-    # increment id tags
-    max_article_id = df_dict['train.csv']['Article_ID'].max() + 1
-    max_ner_tag_id = df_dict['train.csv']['NER_Tag_ID'].max() + 1
-    increment_article_id = partial(increment_id, increment=max_article_id)
-    increment_tag_id = partial(increment_id, increment=max_ner_tag_id)
+    # # increment id tags
+    # max_article_id = df_dict['train.csv']['Article_ID'].max() + 1
+    # max_ner_tag_id = df_dict['train.csv']['NER_Tag_ID'].max() + 1
+    # increment_article_id = partial(increment_id, increment=max_article_id)
+    # increment_tag_id = partial(increment_id, increment=max_ner_tag_id)
 
-    df_dict['validation.csv']['Article_ID'] = df_dict['validation.csv']['Article_ID'].apply(increment_article_id)
-    df_dict['validation.csv']['NER_Tag_ID'] = df_dict['validation.csv']['NER_Tag_ID'].apply(increment_tag_id)
+    # df_dict['validation.csv']['Article_ID'] = df_dict['validation.csv']['Article_ID'].apply(increment_article_id)
+    # df_dict['validation.csv']['NER_Tag_ID'] = df_dict['validation.csv']['NER_Tag_ID'].apply(increment_tag_id)
 
-    # combine train/validation set
-    train_val_df = pd.concat((df_dict['train.csv'], df_dict['validation.csv']), axis=0)
+    # # combine train/validation set
+    # train_val_df = pd.concat((df_dict['train.csv'], df_dict['validation.csv']), axis=0)
 
     # create training dictionary
-    train_dict = create_train_dict(train_val_df)
+    train_dict = create_train_dict(df_dict['train.csv'])
 
     # get max key length
     max_key_len = max([len(x) for x in list(train_dict.keys())])
 
     # transform test sentences
     test_sentences_df = df_dict['test.csv'].groupby(
-        ['Article_ID','Sentence_ID'], as_index=False).agg(Sentence=('Token', list))
+        ['Article_ID', 'Sentence_ID'],
+        as_index=False).agg(Sentence=('Token', list))
     # test_sentence = test_sentences_df.iloc[0]['Sentence']
     # pred_sentence = get_tags(train_dict, test_sentence, max_key_len)
-    get_tags_partial = partial(get_tags, train_dict=train_dict, max_key_len=max_key_len)
-    test_sentences_df['Tagged_Sentence'] = test_sentences_df['Sentence'].apply(lambda x: get_tags_partial(sentence=x))
-    test_sentences_df = test_sentences_df[['Article_ID', 'Sentence_ID', 'Tagged_Sentence']]
+    get_tags_partial = partial(get_tags,
+                               train_dict=train_dict,
+                               max_key_len=max_key_len)
+    test_sentences_df['Tagged_Sentence'] = test_sentences_df['Sentence'].apply(
+        lambda x: get_tags_partial(sentence=x))
+    test_sentences_df = test_sentences_df[[
+        'Article_ID', 'Sentence_ID', 'Tagged_Sentence'
+    ]]
     test_sentences_df = test_sentences_df.explode('Tagged_Sentence')
-    test_sentences_df[['Token', 'NER_Tag_Prediction']] = test_sentences_df['Tagged_Sentence'].apply(pd.Series)
+    test_sentences_df[[
+        'Token', 'NER_Tag_Prediction'
+    ]] = test_sentences_df['Tagged_Sentence'].apply(pd.Series)
     test_sentences_df = test_sentences_df.drop(columns=['Tagged_Sentence'])
     # test_sentences_df.groupby('NER_Tag_Prediction').count()
-    test_preds_df = pd.concat((df_dict['test.csv'], test_sentences_df[['NER_Tag_Prediction']].reset_index(drop=True)), axis=1)
-    print(classification_report(test_preds_df['NER_Tag_Normalized'], test_preds_df['NER_Tag_Prediction']))
-    breakpoint()
+    test_preds_df = pd.concat(
+        (df_dict['test.csv'], test_sentences_df[['NER_Tag_Prediction'
+                                                 ]].reset_index(drop=True)),
+        axis=1)
+    print(
+        classification_report(test_preds_df['NER_Tag_Normalized'],
+                              test_preds_df['NER_Tag_Prediction']))
 
 
 if __name__ == '__main__':