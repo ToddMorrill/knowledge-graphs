9cfe347b145c3f299bc23f17908e3415d0f5fcd0
ToddMorrill <tmorrill12@gmail.com>
2021-02-24 18:22:23 -0500

    V1 TextRank implementation

--- kg/ner/unsupervised.py
+++ kg/ner/unsupervised.py
@@ -30,6 +30,7 @@ from collections import Counter
 import itertools
 
 import string
+from typing import final
 
 import networkx
 import nltk
@@ -39,6 +40,7 @@ nltk.download('conll2000')  # noun phrase evaluation
 nltk.download('stopwords')  # stopwords
 from nltk.corpus import conll2000
 import numpy as np
+from scipy import stats
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 
 import kg.ner.utils as utils
@@ -48,16 +50,19 @@ class NounPhraseDetection(nltk.RegexpParser):
     def __init__(self, grammar=r'NP: {<[CDJNP].*>+}'):
         super().__init__(grammar)
 
-    def extract(self, text, preprocess=True, single_word_proper_nouns=True):
+    def extract(self,
+                document,
+                preprocess=True,
+                single_word_proper_nouns=True):
         # exclude candidates that are stop words or entirely punctuation
         punct = set(string.punctuation)
         stop_words = set(nltk.corpus.stopwords.words('english'))
 
         # optionally preprocess (tokenize sentences/words, tag POS)
         if preprocess:
-            preprocessed_sentences = utils.preprocess(text)
+            preprocessed_sentences = utils.preprocess(document)
         else:
-            preprocessed_sentences = text
+            preprocessed_sentences = document
 
         # extract
         noun_phrases = []
@@ -119,9 +124,26 @@ class EntityScore(object):
     def __init__(self, parser) -> None:
         self.parser = parser
 
-    def candidates(self, text, preprocess=True, single_word_proper_nouns=True):
-        tagged_phrases = self.parser.extract(text, preprocess,
+    def candidates(self,
+                   document,
+                   preprocess=True,
+                   single_word_proper_nouns=True):
+        tagged_phrases = self.parser.extract(document, preprocess,
                                              single_word_proper_nouns)
+
+        return tagged_phrases
+
+    def candidates_documents(self,
+                             documents,
+                             preprocess=True,
+                             single_word_proper_nouns=True):
+        tagged_phrases = []
+        for document in documents:
+            phrases = self.candidates(document, preprocess,
+                                      single_word_proper_nouns)
+            for phrase in phrases:
+                tagged_phrases.append(phrase)
+
         return tagged_phrases
 
     def fit(self, documents, preprocess=True):
@@ -172,76 +194,113 @@ class TFIDFScore(EntityScore):
             return list(zip(phrases, flags, scores))
         return list(zip(phrases, scores))
 
+
 class TextRankScore(EntityScore):
-    # implement TF-IDF/TextRank filters
-    def __init__(self, parser) -> None:
+    # adjectives, nouns, verbs
+    def __init__(self,
+                 document,
+                 preprocess,
+                 parser,
+                 included_pos=['JJ', 'NN', 'VB'],
+                 window_size=2) -> None:
         super().__init__(parser)
+        if preprocess:
+            self.document = utils.preprocess(document)
+        else:
+            self.document = document
+        self.included_pos = included_pos
+        self.window_size = window_size
 
-    def create_nodes_edges(self, documents, preprocess=True):
-        pass
+    def _keep_token(self, token_pos):
+        token, pos = token_pos
+        for included in self.included_pos:
+            if pos.startswith(included):
+                return True
+        return False
 
-    def create_graph(self):
-        pass
+    def _get_nodes(self):
+        # TODO: lemmatize tokens
+        nodes = []
+        for sentence in self.document:
+            for token, pos in sentence:
+                if self._keep_token((token, pos)):
+                    nodes.append((token.lower(), pos))
+        return nodes
 
-    def fit(self, documents, preprocess=True):
-        words = set()
-        bigrams = []
-        for doc in documents:
-            if preprocess:
-                doc = utils.preprocess(doc)
-            for sentence in doc:
-                # retrieve just the tokens
-                token_list = list(zip(*sentence))[0]
-                words.update(set(token_list))
-                for pair in nltk.bigrams(token_list):
-                    bigrams.append(pair)
+    def _get_edges(self):
+        edges = []
+        for sentence in self.document:
+            filtered_sentence = []
+            for token, pos in sentence:
+                if self._keep_token((token, pos)):
+                    filtered_sentence.append((token.lower(), pos))
 
-        # fix the order
-        words = sorted(list(words))
+            for hop in range(self.window_size):
+                for idx, node in enumerate(filtered_sentence[:-1 - hop]):
+                    neighbor = filtered_sentence[hop + idx + 1]
+                    edges.append((node, neighbor))
 
-        # Sort the combinations so that A,B and B,A are treated the same
-        bigrams = [tuple(sorted(d)) for d in bigrams]
+        # include weight on the edge: (2, 3, {'weight': 3.1415})
+        weighted_edges = [(*edge, {
+            "weight": weight
+        }) for edge, weight in Counter(edges).items()]
 
-        # count the combinations
-        counts = Counter(bigrams)
+        return weighted_edges
 
+    def fit(self):
+        # initalize graph
+        self.graph = networkx.DiGraph()
 
-        # Create the table
-        table = np.zeros((len(words),len(words)))
+        # get nodes
+        self.graph.add_nodes_from(self._get_nodes())
 
-        for i, v1 in enumerate(words):
-            for j, v2 in enumerate(words[i:]):        
-                j = j + i 
-                table[i, j] = counts[v1, v2]
-                table[j, i] = counts[v1, v2]
+        # get edges
+        self.graph.add_edges_from(self._get_edges())
 
-        # Display the output
-        print(table)
-        breakpoint()
+        # get scores for words
+        self.token_scores = networkx.pagerank(self.graph)
 
-    def score_phrases(self, phrases, noun_phrase_flags=True):
+    def score_phrases(self,
+                      phrases,
+                      noun_phrase_flags=True,
+                      preprocess=True):
         if noun_phrase_flags:
             phrases, flags = zip(*phrases)
+        
+        # NB: should really be returning pos tags from self.candidates
+        # possible to introduce pos errors when working on these fragments
+        # get pos tags
+        phrases = utils.tokenize_sentences(phrases)
+        phrases = utils.tag_pos(phrases)
 
-        tfidf_vectors = self.vectorizer.transform(phrases)
+        scores = []
+        for phrase in phrases:
+            phrase_score = 0
+            tokens_found = 0
+            for token, pos in phrase:
+                if (token.lower(), pos) in self.token_scores:
+                    phrase_score += self.token_scores[(token.lower(), pos)]
+                    tokens_found += 1
+            # score normalized by length
+            score = phrase_score / tokens_found if tokens_found > 0 else 0
+            scores.append(score)
 
-        # average non-zero entries
-        # row sums
-        sums = np.squeeze(np.asarray(tfidf_vectors.sum(axis=1)))
+        # define a percentile rank for noun phrases (0 for non-noun phrases)
+        noun_phrase_scores = []
+        for idx, phrase in enumerate(phrases):
+            if flags[idx]:
+                noun_phrase_scores.append(scores[idx])
+        
+        final_ranks = []
+        for idx, phrase in enumerate(phrases):
+            if flags[idx]:
+                percentile_rank = stats.percentileofscore(noun_phrase_scores, scores[idx])
+                final_ranks.append((phrase, flags[idx], percentile_rank/100))
+            else:
+                # assign a score of 0.0 to non-noun phrases
+                final_ranks.append((phrase, flags[idx], 0.0))                
 
-        # row-wise counts of non-zero entries (from CSR matrix)
-        # non_zero_counts = np.diff(tfidf_vectors.indptr)
-
-        # TODO: find a better way to compute this, accounting for the vocab that vectorizer uses
-        token_counts = self.vectorizer._count_vocab(
-            raw_documents=phrases, fixed_vocab=False)[1].toarray()
-        token_counts = np.squeeze(np.asarray(token_counts.sum(axis=1)))
-
-        scores = sums / token_counts
-
-        if noun_phrase_flags:
-            return list(zip(phrases, flags, scores))
-        return list(zip(phrases, scores))
+        return final_ranks
 
 
 class EntityTypeDetection():
@@ -258,14 +317,14 @@ def main(args):
 
     chunk_parser = NounPhraseDetection()
 
-    sample = 'Here is some sample text. And some more!'
-    noun_phrases = chunk_parser.extract(articles[0])
-    print(noun_phrases)
+    # sample = 'Here is some sample text. And some more!'
+    # noun_phrases = chunk_parser.extract(articles[0])
+    # print(noun_phrases)
 
-    corpus = [
-        'This is the first document.', 'This document is the second document.',
-        'And this is the third one.', 'Is this the first document?'
-    ]
+    # corpus = [
+    #     'This is the first document.', 'This document is the second document.',
+    #     'And this is the third one.', 'Is this the first document?'
+    # ]
     # entity_extractor = TFIDFScore(chunk_parser)
     # # entity_candidates = entity_extractor.candidates(sample)
     # entity_extractor.fit_tfidf(corpus + [sample])
@@ -276,11 +335,19 @@ def main(args):
     #     'Washington'
     # ]
     # results = entity_extractor.score_phrases(noun_phrases)
-    scorer = TextRankScore(chunk_parser)
-    scorer.fit(corpus)
+    scorer = TextRankScore(articles[0], preprocess=True, parser=chunk_parser)
+    scorer.fit()
+    candidates = scorer.candidates(articles[0])
+    scores = scorer.score_phrases(candidates)
     breakpoint()
 
-    # print(entity_candidates)
+    # candidates = scorer.candidates_documents(corpus)
+    # candidates = scorer.candidates(corpus[0])
+    # breakpoint()
+    # candidate_noun_phrases = [tokens for tokens, flag in candidates if flag]
+    # scored_words = scorer.fit_document(candidate_noun_phrases)
+    # scored_candidates = scorer.score_phrases(candidates, scored_words)
+    # breakpoint()
 
 
 if __name__ == '__main__':
